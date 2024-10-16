d71d528723b2f3b2cc9cbb2ebb045b6cca75f436
umutarsln <umut@Umut-MacBook-Air.local>
2024-10-16 10:23:02 +0300

    bq white

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white`}
       >
         <div>{children}</div>
       </body>
